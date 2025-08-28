6134ffcef3d4dfe6c87bd1da1085dae518819fa3
deploynext <projectdeploy1@gmail.com>
2025-08-28 11:56:45 +0530

    try

--- next.config.ts
+++ next.config.ts
@@ -4,9 +4,9 @@ const nextConfig: NextConfig = {
   /* config options here */
   // trailingSlash: true, 
   // output:"export",
-  images:{
-    unoptimized:true,
-  }
+  // images:{
+  //   unoptimized:true,
+  // }
 };
 
 export default nextConfig;
